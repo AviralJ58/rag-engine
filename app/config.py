b7e2127f6b809ae10477a5ee9f83c48f4a3e253e
Aviral Jain <aviraljain58@gmail.com>
2025-10-17 02:07:47 +0530

    Use Qdrant server instead of local storage

--- app/config.py
+++ app/config.py
@@ -7,8 +7,15 @@ load_dotenv()
 
 # Environment Variables
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
+QDRANT_URL = os.getenv("QDRANT_URL")
+QDRANT_API_KEY = os.getenv("QDRANT_API_KEY", "")
 
 # Initialize Clients
 redis_client = Redis.from_url(REDIS_URL, decode_responses=True)
-qdrant_client = QdrantClient(path="qdrant_storage")  # embedded local mode
-# qdrant = QdrantClient(url=os.getenv("QDRANT_URL"))
+
+qdrant_client = QdrantClient(
+    url=QDRANT_URL,
+    api_key=QDRANT_API_KEY,
+)
+
+print(qdrant_client.get_collections())
